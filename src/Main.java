6834262fbf9d0e745d7a4bbbf6e919bbaf8a2137
Noah J Wright 12th GCHS <134968@SWCSD.US>
2025-02-04 14:23:37 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+
+//Project 1 - To do list - Noah Wright - 2/4/25
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<String> todolist = new ArrayList<>();
+
+
+        int action = -1;
+        while (action != 0) {
+            System.out.println("(1) Add a task");
+            System.out.println("(2) Remove a task");
+            System.out.println("(3) Update a task");
+            System.out.println("(4) List all tasks");
+            System.out.println("(0) Exit");
+            action = input.nextInt();
+            input.nextLine();
+
+
+            if (action == 1) {
+                System.out.println("What is the task you would like to add?");
+                todolist.add(input.nextLine());
+            }
+            if (action == 2) {
+                for (String task : todolist) System.out.println(task);;
+                System.out.println("Which task would you like to remove");
+                todolist.remove(input.nextLine());
+
+            }
+        }
+    }
+}
